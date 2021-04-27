032e1540c2dd720168ccfdcd26100195bfc7de49
Taylor Treece <taylor.treece@protonmail.com>
2021-04-27 16:23:28 -0500

    commit tree 20ee85b122600d918b9bff8c848b6bfe7ce81702

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category|
+      if category[:name].presence
+        self.categories << Category.find_or_create_by(name: category[:name])
+      end
+    end
+  end
 
 end
